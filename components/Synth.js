73697b66a86f79cda35626cb0b7e25e246cccb3d
Zach Green <zhgreen@gmail.com>
2018-12-06 05:31:08 +0100

    manage all state in reducer

--- components/Synth.js
+++ components/Synth.js
@@ -3,44 +3,67 @@ import Tone from "tone";
 import { chromaticKeyMap } from "../config";
 const synth = new Tone.PolySynth(6, Tone.Synth).toMaster();
 
-const reducer = ({ attack, release }, action) => {
+const initialState = {
+  activeNotes: [],
+  attack: null,
+  release: null,
+  octave: 2
+};
+const reducer = (state, action) => {
   switch (action.type) {
     case "attack":
       return {
-        attack: action.payload
+        ...state,
+        activeNotes: state.activeNotes.concat(action.payload),
+        attack: action.payload,
+        release: null
+      };
+    case "octave":
+      return {
+        ...state,
+        attack: null,
+        release: null,
+        octave: action.payload
       };
     case "release":
-      return { release: action.payload };
+      return {
+        ...state,
+        activeNotes: state.activeNotes.filter(note => note !== action.payload),
+        release: action.payload,
+        attack: null
+      };
     default:
       return state;
   }
 };
 export default memo(() => {
-  const [state, dispatch] = useReducer(reducer, {});
-  const [activeNotes, setActiveNotes] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { activeNotes, attack, octave, release } = state;
+  const notes = chromaticKeyMap(octave);
   useEffect(
     () => {
-      synth.triggerAttack(state.attack);
-      synth.triggerRelease(state.release);
+      if (attack) {
+        synth.triggerAttack(attack);
+      }
+      if (release) {
+        synth.triggerRelease(release);
+      }
     },
     [state]
   );
   const onKeyDown = ({ key }) => {
-    if (chromaticKeyMap(2)[key] === state.attack) {
+    if (notes[key] === attack) {
       return;
     }
-    dispatch({ type: "attack", payload: chromaticKeyMap(2)[key] });
-    setActiveNotes(notes => {
-      return notes.concat(chromaticKeyMap(2)[key]);
-    });
+    dispatch({ type: "attack", payload: notes[key] });
   };
   const onKeyUp = ({ key }) => {
-    dispatch({ type: "release", payload: chromaticKeyMap(2)[key] });
-    setActiveNotes(notes => {
-      return notes.filter(note => note !== chromaticKeyMap(2)[key]);
-    });
+    dispatch({ type: "release", payload: notes[key] });
   };
-  console.log("activeNotes", activeNotes);
+  const handleOctaveChange = e => {
+    dispatch({ type: "octave", payload: parseInt(e.target.value, 10) });
+  };
+  console.log("state", state);
   return (
     <div className="container" onKeyDown={onKeyDown} onKeyUp={onKeyUp}>
       <style jsx global>{`
@@ -66,9 +89,16 @@ export default memo(() => {
           width: 100vw;
         }
       `}</style>
-      <input type="range" />
+      <input
+        onChange={handleOctaveChange}
+        type="range"
+        min="0"
+        max="4"
+        value={octave}
+        step="1"
+      />
       <div>
-        {Object.entries(chromaticKeyMap(2)).map(([key, val]) => (
+        {Object.entries(notes).map(([key, val]) => (
           <button key={key}>
             <span
               style={{
