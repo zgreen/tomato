698ea20a1acd254f7e09f2a870f2e6a4e29c08e9
Zach Green <zhgreen@gmail.com>
2020-01-03 23:48:10 +0100

    fix: simpler should focus logic

--- components/presentational/Synth.js
+++ components/presentational/Synth.js
@@ -1,23 +1,19 @@
-import { memo, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import css from "styled-jsx/css";
 const { className, styles } = css.resolve`
-div {
-  flex-direction: column;
-  display: flex;
-  flex: 1 0 auto;
-  margin-bottom: 10px;
-  width: 100%;
-}
-`;
-export default memo(({ containerTabIndex, shouldFocus, ...props }) => {
-  const divRef = useRef(null);
-  if (shouldFocus) {
-    console.log("should focus");
-    divRef.current.focus();
+  div {
+    flex-direction: column;
+    display: flex;
+    flex: 1 0 auto;
+    margin-bottom: 10px;
+    width: 100%;
   }
+`;
+const Synth = ({ containerTabIndex, shouldFocus, ...props }) => {
+  const divRef = useRef(null);
   useEffect(() => {
     divRef.current.focus();
-  }, []);
+  }, [shouldFocus]);
   return (
     <>
       {styles}
@@ -28,4 +24,6 @@ export default memo(({ containerTabIndex, shouldFocus, ...props }) => {
       />
     </>
   );
-});
+};
+
+export default Synth;
